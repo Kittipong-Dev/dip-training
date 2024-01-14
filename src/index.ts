34ff80278da5343b612f281271ce272e0ae9f8f9
Kittipong-Dev <achikrab@gmail.com>
2024-01-14 20:59:04 +0700

    add fetch picture

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,31 @@ const port = 3000
 const store = {
     x: 0
 }
+const data = [
+  {
+    img: "https://statusneo.com/wp-content/uploads/2023/02/MicrosoftTeams-image551ad57e01403f080a9df51975ac40b6efba82553c323a742b42b1c71c1e45f1.jpg",
+    title: "1"
+  },
+  {
+    img: "https://miro.medium.com/v2/resize:fit:1400/1*YMJDp-kqus7i-ktWtksNjg.jpeg",
+    title: "2"
+  },
+  {
+    img: "https://static.vecteezy.com/system/header_search_link/thumbnail/34/hsl_large_term-bg-1__1_.jpg",
+    title: "3"
+  }
+]
+
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
+app.get('/api/count', (req, res) => {
   res.json(store)
   store.x +=  1
+})
+
+app.get("/api/data", (req, res) => {
+  res.json(data)
 })
 
 app.get("/welcome", (req, res) => {
