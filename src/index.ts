e787f6bffc19a7a6605e77fb51bd1bd19c941c86
Kittipong-Dev <achikrab@gmail.com>
2024-01-14 10:31:02 +0700

    commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+const store = {
+    x: 0
+}
+
+app.get('/', (req, res) => {
+  res.json(store)
+  store.x +=  1
+})
+
+app.get("/welcome", (req, res) => {
+  res.send("Hello world")
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
+})
